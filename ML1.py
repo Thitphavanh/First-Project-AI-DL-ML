aa6bdba6e645a93c796c06f2964cc0c775b495fb
Hery <thitphavanh.psv@gmail.com>
2022-05-15 21:23:27 +0700

    update

--- ML1.py
+++ ML1.py
@@ -1,9 +1,7 @@
 from sklearn import datasets
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 digit_dataset = datasets.load_digits()
 
 print(digit_dataset.target[0])
-plt.imshow(digit_dataset.images[0],cmap = plt.get_cmap('gray'))
+plt.imshow(digit_dataset.images[0], cmap=plt.get_cmap('gray'))
 plt.show()
-
-
