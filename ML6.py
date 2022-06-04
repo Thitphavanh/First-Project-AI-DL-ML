8c23617c48294a0e4bd2ab81320309e32f2429c3
Hery <thitphavanh.psv@gmail.com>
2022-06-04 22:32:56 +0700

    update

---
+++ ML6.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+rng = np.random
+
+
+# ການຈຳລອງຂໍ້ມູນ
+x = rng.rand(50)*10
+y = 2*x+rng.randn(50)
+
+
+# linear regression model
+model = LinearRegression()
+
+x_new = x.reshape(-1, 1)
+
+# train
+model.fit(x_new, y)
+
+# test model
+xfit = np.linspace(-1, 11)
+xfit_new = xfit.reshape(-1, 1)
+
+
+yfit = model.predict(xfit_new)
+
+
+# analysis model & resutl
+plt.scatter(x, y)
+plt.plot(xfit, yfit)
+plt.show()
