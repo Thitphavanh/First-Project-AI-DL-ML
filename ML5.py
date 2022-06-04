3e726f71825290910de3cf57a2032496c1bac53c
Hery <thitphavanh.psv@gmail.com>
2022-06-04 22:13:31 +0700

    update

--- ML5.py
+++ ML5.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(-5, 5, 100)
-y = 2*x+1
+rng = np.random
+x = rng.rand(50)*10
+
+# a=rng.rand()*100
+y = 2*x+rng.randn(50)
 
 plt.scatter(x,y)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend(loc='upper left')
-plt.title('Grap y=2x+1')
-plt.grid()
 plt.show()
 
