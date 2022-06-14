1e9832591d5802f7bc3679bf6e4b4241ed1468eb
Hery <thitphavanh.psv@gmail.com>
2022-06-14 08:42:46 +0700

    update

--- ML9.py
+++ ML9.py
@@ -12,9 +12,16 @@ mnist = {
 # 1 - 60000
 # 60001 - 70000
 x, y = mnist["data"], mnist["target"]
+# train & test set
+# class 0 - 9
 x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
 
 # 0 - 9
 # 5000
 # 5
 # True, False
+
+# class 0 ບໍ່ແມ່ນ class 0
+# ຂໍ້ມູນ 1 ຄ່າ -> class 0 ຫຼືບໍ? True : False
+predict = 5000
+y_train_0 = (y_train == 0)
