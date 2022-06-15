ae7b09d28d48c2d700c74ddf84531bec9c160ae9
Hery <thitphavanh.psv@gmail.com>
2022-06-16 05:17:44 +0700

    update

--- ML10.py
+++ ML10.py
@@ -1,6 +1,18 @@
 from scipy.io import loadmat
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
+
+
+def displayImage(x):
+    plt.imshow(x.reshape(28, 28), cmap=plt.cm.binary, interpolation='nearest')
+    plt.show()
+
+
+def displayPredict(clf, actually_y, x):
+    print('Actually = ', actually_y)
+    print('Prediction = ', clf.predict([x])[0])
+
 
 mnist_raw = loadmat("mnist-original.mat")
 mnist = {
@@ -24,11 +36,16 @@ x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
 # class 0 ບໍ່ແມ່ນ class 0
 # ຂໍ້ມູນຄ່າ 5000 -> model -> class 0 ຫຼືບໍ? True : False
 # y_train = [0,0,........,9....,9]
-predict_number = 5000
+predict_number = 500
 y_train_0 = (y_train == 0)
 y_test_0 = (y_test == 0)
 
 # y_train_0 = [true,true,........,false....,false]
 
-print(y_train_0.shape, y_train_0)
-print(y_test_0.shape, y_test_0)
+
+sgd_clf = SGDClassifier()
+sgd_clf.fit(x_train, y_train_0)
+
+
+displayPredict(sgd_clf, y_test_0[predict_number], x_test[predict_number])
+displayImage(x_test[predict_number])
