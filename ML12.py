32705b6f0c2806c75cd1577f3784a5164436e768
Hery <thitphavanh.psv@gmail.com>
2022-06-21 06:01:16 +0700

    update

--- ML12.py
+++ ML12.py
@@ -2,4 +2,4 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 iris_dataset = load_iris()
-train_test_split()
+x_train, x_test, y_train, y_test = train_test_split()
