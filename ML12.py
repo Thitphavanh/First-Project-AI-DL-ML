9fed301efbec47176dc192ce430b41e98e5b9167
Hery <thitphavanh.psv@gmail.com>
2022-06-22 05:47:29 +0700

    update

--- ML12.py
+++ ML12.py
@@ -1,5 +1,20 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 iris_dataset = load_iris()
-x_train, x_test, y_train, y_test = train_test_split()
+x_train, x_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], test_size=0.4, random_state=0)
+
+
+# Model
+knn = KNeighborsClassifier(n_neighbors=1)
+
+
+# training
+knn.fit(x_train, y_train)
+
+# prediction
+pred = knn.predict([x_test[0]])
+
+print('ຜົນການພະຍາກອນ', pred)
+print('ທຳນານວ່າຢູ່ໃນກຸ່ມສາຍພັນ', iris_dataset['target_names'][pred])
