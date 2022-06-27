6401a5d9ff03adfcccbe99297b43b6008ee074d1
Hery <thitphavanh.psv@gmail.com>
2022-06-28 06:22:06 +0700

    update

--- ML16.py
+++ ML16.py
@@ -1,31 +1,27 @@
-from cProfile import label
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# load dataset
+iris = load_iris()
+
+# assign attribute, target
+x = iris['data']
+y = iris['target']
 
 
-#  Read Data
-df = pd.read_csv('diabetes.csv')
+# trian, test
+x_trian, x_test, y_train, y_test = train_test_split(x, y)
 
-# Data
-x = df.drop('Outcome', axis=1).values
-
-#  Outcome Data
-y = df['Outcome'].values
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
-
-knn = KNeighborsClassifier(n_neighbors=8)
+# model
+model = GaussianNB()
 
 # train
-knn.fit(x_train, y_train)
+model.fit(x_trian, y_train)
 
 # prediction
-y_pred = knn.predict(x_test)
-print(confusion_matrix(y_test, y_pred))
+y_pred = model.predict(x_test)
 
-print(pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predictions'], margins=True)
-)
+# Accuracy Score
+print("Accuracy =", accuracy_score(y_test, y_pred))
